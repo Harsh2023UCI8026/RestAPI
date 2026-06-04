4e4137439af6438643136b0f04f92940920d92d3
Harsh Jha <harsh.jha.ug23@nsut.ac.in>
2026-06-04 13:20:42 +0530

    Updated code

--- src/main/java/com/telusko/demorest/AlienResource.java
+++ src/main/java/com/telusko/demorest/AlienResource.java
@@ -8,27 +8,53 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.PathParam;
+
 
 @Path("aliens")
 public class AlienResource {
 
     AlienRepository repo = new AlienRepository();
 
+    // GET /aliens
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Alien> getAliens() {
+        return repo.getAliens();
+    }
+    
+    @GET
+    @Path("alien/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Alien getAliens(@PathParam("id") int id) {
+        return repo.getAlien(id);
+    }
+
+    // GET /aliens/alien
     @GET
     @Path("alien")
     @Produces(MediaType.APPLICATION_XML)
     public List<Alien> getAlien() {
         return repo.getAliens();
     }
+    
 
+    // POST /aliens
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Alien createAlien1(Alien a) {
+        repo.create(a);
+        return a;
+    }
+
+    // POST /aliens/alien
     @POST
     @Path("alien")
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
-    public Alien createAlien(Alien a) {
-
+    public Alien createAlien2(Alien a) {
         repo.create(a);
-
         return a;
     }
 }
